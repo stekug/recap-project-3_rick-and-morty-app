3ab192261ae19e64eb2179a1703ea3a918719890
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-06-19 16:29:57 +0200

    refactor CharacterFunction, add src folder, refactor mochup data

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import CharacterCard from './components/CharacterCard/CharacterCard.js';
+import { expectedData } from './src/mochupData.js';
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector('[data-js="search-bar-container"]');
 const searchBar = document.querySelector('[data-js="search-bar"]');
@@ -11,40 +14,6 @@ const maxPage = 1;
 const page = 1;
 const searchQuery = '';
 
-// mochUp Data, can be deleted later...
-const expectedData = {
-    id: 3,
-    name: 'Rick Sanchezzzzz',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-    status: 'Alive',
-    type: '',
-    episode: ['aojlk', 'opikajmskd', 'oasjnd'],
-};
-
-function CharacterCard({ id, name, image, status, type, episode }) {
-    const liElement = document.createElement('li');
-    console.log(image);
-    liElement.classList.add('card');
-    liElement.innerHTML = `<div class="card__image-container">
-  <img
-    class="card__image"
-    src="${image}"
-    alt="${name}"
-  />
-  <div class="card__image-gradient"></div>
-</div>
-<div class="card__content">
-  <h2 class="card__title">${name}</h2>
-  <dl class="card__info">
-    <dt class="card__info-title">Status</dt>
-    <dd class="card__info-description">${status}</dd>
-    <dt class="card__info-title">Type</dt>
-    <dd class="card__info-description">${type}</dd>
-    <dt class="card__info-title">Occurrences</dt>
-    <dd class="card__info-description">${episode.length}</dd>
-  </dl>
-</div>`;
-    return liElement;
-}
-
-cardContainer.append(CharacterCard(expectedData));
+expectedData.forEach((character) => {
+    cardContainer.append(CharacterCard(character));
+});
